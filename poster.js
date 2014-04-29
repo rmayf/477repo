98ed200d7d3d3f0b0c5f6546ae9ae2e0996bbc58
Reid <rmayf@uw.edu>
2014-04-29 12:59:07 -0700

    added id to url

--- poster.js
+++ poster.js
@@ -1,9 +1,10 @@
 var http = require('http');
 var fs = require('fs');
+var id = parseInt('deadbabe', 16);
 
 var req = http.request({ hostname: 'klement.cs.washington.edu',
                port: 4774,
-               path: '/upload?filename=dumb.wav',
+               path: '/upload?filename=dumb.wav&id='+id,
                method: 'POST' }, function(res) {
   console.log('STATUS: ' + res.statusCode);
   console.log('HEADERS: ' + JSON.stringify(res.headers));
