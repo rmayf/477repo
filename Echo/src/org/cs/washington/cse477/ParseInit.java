c478055a7b9b4c84b2d56e92457382a1fb75d8e0
isaiahmathieu <isaiah.mathieu@gmail.com>
2014-05-05 14:52:56 -0700

    added the file that registers the device with Parse for pushes

---
+++ Echo/src/org/cs/washington/cse477/ParseInit.java
@@ -0,0 +1,20 @@
+package org.cs.washington.cse477;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+import com.parse.PushService;
+
+public class ParseInit extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		// Add your initialization code here
+		Parse.initialize(this, "t1oV8LeSRZsCFmBSe0yudiZv17eHIJdaHtytj0ZP", "CgnLN9dMbywD4CXELdVcAOxV8FFC6La6c1sYRV0S");
+		PushService.setDefaultPushCallback(this, NotificationActivity.class);
+		ParseInstallation.getCurrentInstallation().saveInBackground();
+	}
+
+}
