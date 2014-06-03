f70e674f3c3bb6107c457c6876cb87927451bea5
rmayf <rmayf@uw.edu>
2014-06-02 17:20:56 -0700

    taskFlyport, heard some audio

--- taskFlyport.c
+++ taskFlyport.c
@@ -22,6 +22,8 @@
 #define UPLOAD_HEADER_LEN 11
 #define TXBUF_SIZE 2048
 #define MAGIC_NUM 0xdeadbabe
+#define SAMPLE_FREQ 0x2B11 // 11025 Hz
+#define BIT_DEPTH 16
 #define OPT_SETUP 2
 #define OPT_UPLOAD 1
 #define ENVELOPE_HISTORY_SIZE 1024
@@ -33,7 +35,6 @@
 #define ENVELOPE_FIXED_THRES 60
 #define MEM_SEQUENTIAL_READ 0x3
 #define MEM_SEQUENTIAL_WRITE 0x2
-#define MEM_SIZE 65536 // Bytes
 #define SPI_CLK_PIN p18
 #define SPI_OUT_PIN p17
 #define SPI_IN_PIN p7
@@ -44,6 +45,12 @@
 BYTE txbuf[TXBUF_SIZE];
 BOOL ParamSet = FALSE;
 unsigned long long counter = 0;
+const unsigned long long k = 60000;
+const UINT16 half_max = 0xFFC0 / 2;
+
+int testI = 0;
+
+BOOL FLAG = TRUE;
 
 void reset() {
   _dbprint("Resest Button Pushed\r\n");
@@ -55,10 +62,38 @@ void reset() {
 void __attribute__((__interrupt__, auto_psv)) _T4Interrupt( void )
 {
   TMR4 = 0;// interrupt counter reinitialized
-  WORD val = (WORD)ADCVal(1);
-  cSPIWriteNextWORDSeq(val);
-  counter += 2;
+  
+  FLAG = FALSE;
+  
   IFS1bits.T4IF = 0;//clear interrupt flag
+  T4CON = 0;
+  
+  /*
+  testI++;
+  WORD val = (((WORD)ADCVal(1)) << 6) - half_max;
+  counter += 2;
+  */
+  /*
+  WORD val = (((WORD)ADCVal(1)) << 6) - half_max;
+  cSPIWriteNextWORDSeq((WORD)val);
+  counter += 2;
+  
+  IFS1bits.T4IF = 0;//clear interrupt flag
+  if ( counter >= k )
+  {
+    T4CON = 0;
+	IEC1bits.T4IE = 0;
+	T4CONbits.TON = 0;
+  }
+  */
+}
+
+void stop_sampling() {
+	FLAG = FALSE;
+  char stupid[40];
+  sprintf(stupid, "Sample Rate: %u\r\n ", testI);
+  UARTWrite(1, "*******************************\r\n");
+  UARTWrite(1, stupid);
 }
 
 void FlyportTask() {
@@ -76,6 +111,7 @@ void FlyportTask() {
   T4CON = 0;  //turn off timer(good practise!)        
   T4CONbits.TCKPS = 0; //clock divider=1
   PR4 = 0x5AB; //limit to raise interrupt=1451
+  //PR4 = 0x1E84800;
   TMR4 = 0; // init timer counter value
   IFS1bits.T4IF = 0; //interrupt flag off
 
@@ -152,8 +188,7 @@ void FlyportTask() {
 			// weird type shit was going on with the sampling loop
 			// how many bits is int, unsigned, unsigned long, long??
 			// using unsigned long long appears to fix the problem and the ADC samples correct number of times
-			unsigned long long bytes_written;
-			unsigned long long k = 65536;
+			unsigned long long bytes_written = 0;
 			// start sequential write
 			cSPIStartSeqWrite(0, k);
 			char msg3[40];
@@ -161,20 +196,49 @@ void FlyportTask() {
 			_dbprint(msg3);
 			_dbprint("Before loop\r\n");
 			
+			//WFHibernate();
+		    vTaskSuspendAll();
+			
+			
+			struct tm dumb;
+		    RTCCSet(&dumb);
+		    RTCCAlarmConf(&dumb, REPEAT_INFINITE/*REPEAT_NO*/, EVERY_SEC, stop_sampling);
+			RTCCAlarmSet(TRUE);
+			
+			
 			// Start Timer
-            IEC1bits.T4IE = 1; //interrupt activated
-			T4CONbits.TON = 1; // timer start
+            //IEC1bits.T4IE = 1; //interrupt activated
+			//T4CONbits.TON = 1; // timer start
+			
+			RTCCAlarmSet(TRUE);
+			
+			while (FLAG)
+			{
+				//WORD dummy = (((WORD)ADCVal(1)) << 6) - half_max;
+				WORD dummy = (WORD)ADCVal(1);
+				BYTE b = (dummy>>2) & 0xFF;
+				//cSPIWriteNextWORDSeq((WORD)dummy);
+				cSPIWriteNextBYTESeq(b);
+				//counter += 2;
+				counter++;
+			}
+			
+			
+			
 			
 			//vTaskDelay();
-			for (counter = 0; counter < k;);\
-			//xTaskResumeAll();
+			//for (counter = 0; counter < k;);
+			
+			xTaskResumeAll();
 			
 			_dbprint("After loop\r\n");
 			char msg2[40];
+			
 			sprintf(msg2, "loop ran %llu times\r\n", counter/2);
 			_dbprint(msg2);
 			cSPIEndSeqWrite(&bytes_written);
 		
+			
 			/* DEBUG */
 			cSPIsetMODE(MODE_BYTE);
 			for (counter = 0; counter < k/16384; counter++) {
@@ -185,6 +249,7 @@ void FlyportTask() {
 				_dbprint(m);
 			}
 			
+			//WFOn();
 			
 			_dbprint("Done Sampling\r\n");
 			char m[40];
@@ -202,27 +267,25 @@ void FlyportTask() {
 			_dbprint("Connected to the server\r\n");
 			memcpy(txbuf, &magic_num, 4);
 			txbuf[4] = OPT_UPLOAD;
-			memcpy(&txbuf[5], /*TODO &sampling_rate*/&magic_num, 4);
-			txbuf[9] = 8;
+			memcpy(&txbuf[5], SAMPLE_FREQ , 4); // SAMPLE_FREQ = 0x2B11 = 11025 Hz
+			txbuf[9] = BIT_DEPTH; // BIT_DEPTH = 16
 			txbuf[10] = 1;
+			TCPWrite(server, txbuf, UPLOAD_HEADER_LEN);
 			
-			
-			txbuf_idx = UPLOAD_HEADER_LEN;
 			amount_read = 0;
 			unsigned long long remaining = bytes_written;
-			
+			cSPIsetMODE(MODE_SEQ);
 			while (remaining > 0) {
 				int to_read = TXBUF_SIZE;
 				if (remaining < TXBUF_SIZE) {
 					to_read = remaining;
 				}
-				cSPIReadSeq((WORD)amount_read, (unsigned long)to_read, &txbuf[txbuf_idx]);
-				txbuf_idx = 0;
+				cSPIReadSeq((WORD)amount_read, (unsigned long)to_read, txbuf);
 				
 				TCPWrite(server, txbuf, to_read);
 				
 				remaining -= to_read;
-				amount_read += (TXBUF_SIZE - txbuf_idx);
+				amount_read += to_read;
 			  
 				_dbprint("Wrote a chunk\r\n");
 			}
