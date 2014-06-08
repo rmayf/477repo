ab589fb96faaf64b20914cf89b0ca55f6e8f592f
isaiahmathieu <isaiah.mathieu@gmail.com>
2014-06-08 13:12:40 -0700

    notification deletion works. haven't tested it thoroughly but its pretty much the same code as deletion of match targets

--- Echo/src/org/cs/washington/cse477/NotificationActivity.java
+++ Echo/src/org/cs/washington/cse477/NotificationActivity.java
@@ -2,7 +2,15 @@ package org.cs.washington.cse477;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
+import org.cs.washington.cse477.ConfirmDeleteDialog.ConfirmDeleteListener;
+
+import android.app.DialogFragment;
+import android.app.FragmentManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -11,12 +19,15 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.parse.DeleteCallback;
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
-public class NotificationActivity extends ActionBarActivity {
+public class NotificationActivity extends ActionBarActivity 
+								implements ConfirmDeleteListener {
 	// logging tag
 	private static final String LOG_TAG = "NotificationActivity";
 	
@@ -26,6 +37,7 @@ public class NotificationActivity extends ActionBarActivity {
 	protected ListView mNotifyView;
 	protected static ArrayAdapter<ParseObject> mNotifyAdapter;
 	protected static List<ParseObject> mParseNotifications;
+	protected final FragmentManager mFragmentManager = getFragmentManager();
 	
 	/**
 	 * initialize and fetch notifications
@@ -41,7 +53,7 @@ public class NotificationActivity extends ActionBarActivity {
 		 */
 		mParseNotifications = new ArrayList<ParseObject>(MAX_REFRESH);
 		mNotifyView = (ListView) findViewById(R.id.notification_listview);
-		mNotifyAdapter = new NotificationListAdapter(this, mParseNotifications);
+		mNotifyAdapter = new NotificationListAdapter(this, mParseNotifications, mFragmentManager);
 		mNotifyView.setAdapter(mNotifyAdapter);
 		
 	}
@@ -83,6 +95,74 @@ public class NotificationActivity extends ActionBarActivity {
 			}
 		});
 	}
+	
+    /**
+     * click handler for delete dialog -> confirm delete
+     */
+    @Override
+	public void onDeleteDialogPositiveClick(DialogFragment dialog) {
+		Log.v(LOG_TAG,"positive click delete dialog");
+		Bundle args = dialog.getArguments();
+		if (args != null) {
+			String toDelete = args.getString("filename");
+			String objId = args.getString("objectId");
+			deleteEventFromParse(objId); 
+			deleteEventFromNode(toDelete);
+		}
+    }
+    
+    private void deleteEventFromParse(String objId) {
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("Event");
+		query.whereEqualTo("objectId", objId);
+		query.getFirstInBackground(new GetCallback<ParseObject>() {
+			
+			@Override
+			public void done(ParseObject obj, ParseException e) {
+				if (e == null) {
+					Log.v(LOG_TAG, "attempting to delete object");
+					obj.deleteInBackground(new DeleteCallback() {
+						
+						@Override
+						public void done(ParseException delE) {
+							if (delE == null) {
+								Log.v(LOG_TAG, "successfully deleted object");
+								refreshNotifications();
+							} else {
+								Log.e(LOG_TAG, "error deleting object");
+							}
+						}
+					});
+				} else {
+					Log.v(LOG_TAG, "failed to find matching object to delete");
+				}
+			}
+		});   	
+    }
+    
+	private void deleteEventFromNode(String toDelete) {
+		String urlAndPath = "http://" + AppInit.host + ":" + AppInit.port + "/deleteEventRecording?filename=";
+		urlAndPath += toDelete;
+		//urlAndPath += ".wav";
+		AsyncTask<String,Void,Integer> httpReq = new AsyncHttpRequestToMatchServer();
+		int statusCode = -1;
+		try {
+			statusCode = httpReq.execute(urlAndPath).get(10, TimeUnit.SECONDS).intValue();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (statusCode != -1) {
+			Log.v(LOG_TAG, "status code from http response: " + statusCode);
+		} 
+	}
+
+	
 
 	/**
 	 * add menu to view
--- Echo/src/org/cs/washington/cse477/OnClickListenerWithArgs.java
+++ Echo/src/org/cs/washington/cse477/OnClickListenerWithArgs.java
@@ -3,13 +3,22 @@ package org.cs.washington.cse477;
 import android.view.View;
 
 public abstract class OnClickListenerWithArgs implements View.OnClickListener {
-	private String str;
+	private String str = null;
+	private String objectId = null;
 	
 	public OnClickListenerWithArgs(String str) {
 		this.str = str;
 	}
+	public OnClickListenerWithArgs(String filename, String objectId) {
+		str = filename;
+		this.objectId = objectId;
+	}
 	
 	public String getString() {
 		return str;
+	}
+	
+	public String getObjectIdString() {
+		return objectId;
 	}
 }
